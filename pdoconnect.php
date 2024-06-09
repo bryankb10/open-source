ce13209a533442dc81239347c882e83e120d42ac
Christy-112 <dechristy.huang@gmail.com>
2024-06-09 22:26:56 +0800

    Update pdoconnect.php

--- pdoconnect.php
+++ pdoconnect.php
@@ -4,4 +4,6 @@
     $username = "root";
     $password = "";
 
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
