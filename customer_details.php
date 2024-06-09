a7ce0e89e1ca358b1745d2ad394768e3927502c6
CritZs <84637996+CritZs@users.noreply.github.com>
2024-06-10 04:36:04 +0800

    Update customer_details.php

--- customer_details.php
+++ customer_details.php
@@ -14,29 +14,117 @@
             justify-content: center;
             text-align:center;
             flex-direction: column;
+            font-size:2vw;
         }
         form {
             text-align: center;
-            margin-top: 20px;
+            margin-top: 2vw;
         }
         input[type=text], input[type=tel], input[type=number] {
             display: block;
-            margin: 10px auto;
-            padding: 10px;
+            margin: 0.5vw auto;
+            padding: 0.5vw;
+            font-size: 1.5vw;
         }
         input[type=submit] {
-            padding: 10px 20px;
-            margin-top: 20px;
+            padding: 0.5vw 1vw;
+            margin-top: 1vw;
+            font-size: 2vw;
         }
         h2 {
             width:100%;
-            height:40px;
+            height:4vw;
             background-color:brown;
             font-family:'Verdana';
             font-weight:bold;
+            font-size: 3vw;
             text-align:center;
             color:yellow;
             margin:0;
+        }
+        .table {
+            margin-left: 37vw; 
+            border: 0.5vw solid black;
+            border-radius: 1.5vw 1.5vw 1.5vw 1.5vw;
+            background: whitesmoke;
+            width: 24vw;
+            height: 24vw;
+        }
+        .button-container {
+            display: flex;
+            margin-top: 1vw;
+            margin-left: 44.1vw;
+        }
+        .button {
+            font-family:'kaisei-tokumin';
+            font-size:2vw;
+            width: 10vw;
+            padding: 0.1vw;
+            cursor: pointer;
+            text-align: center;
+            background-color: #d3d3d3;
+            border: none;
+            border-radius: 0.5vw;
+            position:absolute;
+            left: 
+        }
+        .button:hover {
+            background-color: #c0c0c0;
+        }
+        .button-container {
+            display: flex;
+            margin-top: 1vw;
+            margin-left: 44.1vw;
+        }
+        .textbox {
+            font-family: 'kaisei-tokumin';
+            font-size: 2vw;
+            width: 19vw; /* Adjust width to fit a single word */
+            text-align: center;
+            background-color: #d3d3d3;
+            border: none;
+            border-radius: 0.5vw;
+        }
+
+        .textbox-container {
+            justify-content: center; /* Center the textbox */
+            margin-top: 1vw;
+            align: center;
+        }
+        .container {
+            display: flex;
+            align-items: center;
+            cursor: pointer;
+            font-size: 2vw;
+            user-select: none;
+        }
+
+        .container input {
+            position: absolute;
+            opacity: 0;
+            cursor: pointer;
+        }
+
+        /* Create a custom radio button */
+        .checkmark {
+            display: inline-block;
+            width: 1.5vw;
+            height: 1.5vw;
+            background-color: #eee;
+            border-radius: 50%;
+            margin-top: 1vw;
+            margin-left: 2vw;
+            position: relative;
+        }
+
+        /* On mouse-over, add a grey background color */
+        .container:hover input ~ .checkmark {
+            background-color: #ccc;
+        }
+
+        /* When the radio button is checked, add a blue background */
+        .container input:checked ~ .checkmark {
+            background-color: #2196F3;
         }
     </style>
 </head>
@@ -59,13 +147,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['save'])) {
 
 echo "<h2>Enter Your Details</h2>
 <form method='POST'>
-    Name: <input type='text' name='name' placeholder='Name' required>
-    Phone number: <input type='tel' name='phoneNo' placeholder='Phone Number' required>
-    LineID: <input type='text' name='lineid' placeholder='LineID' required>
-    <div class='radio-group'>
-        <label><input type='radio' name='order_type' id='dine_in' value='Dine In' required> Dine In</label>
-        <label><input type='radio' name='order_type' id='take_away' value='Take Away' required> Take Away</label>
-    </div><br>
-    <input type='submit' name='save' value='Submit'>
+    <div class='table textbox-container'>
+    Name: <input type='text' class='textbox' name='name' placeholder='Name' required>
+    Phone number: <input type='tel' class='textbox' name='phoneNo' placeholder='Phone Number' required>
+    LineID: <input type='text' class='textbox' name='lineid' placeholder='LineID' required>
+    <div class='radio-group container'>
+        <label><input type='radio' name='order_type' id='dine_in' value='Dine In' required><span class='checkmark'></span> Dine In</label>
+        <label><input type='radio' name='order_type' id='take_away' value='Take Away' required><span class='checkmark'></span> Take Away</label>
+    </div></div><br>
+    <div class='button-container'>
+    <input type='submit' class='button' name='save' value='Submit'>
+    </div>
 </form>";
 ?>
+
