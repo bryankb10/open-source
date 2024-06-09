00b7d752fee98136ed1d1d3f8fc27d55b401a582
bryankb10 <bk.bryankenneth@gmail.com>
2024-06-10 00:54:33 +0800

    Update pdoconnect.php

--- pdoconnect.php
+++ pdoconnect.php
@@ -1,7 +1,7 @@
 <?php
     $host = "localhost";
     $dbname = "mydb";
-    $username = "mydb";
+    $username = "myapp";
     $password = "1234";
 
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
