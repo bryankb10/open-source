a8e91c21cd34eb6469a5ec4825ce374a0ef8448f
bryankb10 <bk.bryankenneth@gmail.com>
2024-06-10 00:50:54 +0800

    Update pdoconnect.php

--- pdoconnect.php
+++ pdoconnect.php
@@ -1,8 +1,8 @@
 <?php
     $host = "localhost";
     $dbname = "mydb";
-    $username = "root";
-    $password = "";
+    $username = "mydb";
+    $password = "1234";
 
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
